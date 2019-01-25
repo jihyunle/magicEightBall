91780683fca4f2fee2b954a3cc61b3a29fd50a8a
Jennifer Lee <jennifer.jlee@outlook.com>
2019-01-25 11:53:53 -0500

    Called a new method from main. Deleted old method. New method uses arrys instead of switch statements

--- src/magicEightBall.java
+++ src/magicEightBall.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class magicEightBall {
+
     public static void main(String[]args){
 
         // Ask if user wants to play
@@ -19,19 +20,14 @@ public class magicEightBall {
             System.out.println("What is your question?");
             String userQuestion = input.nextLine();
             System.out.println();
-
             System.out.println("YOU ASKED: " + userQuestion + "\n");
 
-            // Generate a random number between 1 and 20
-            Random rnd = new Random();
-            int number = 0;
-            number = rnd.nextInt(20)+1;
-
-            // Call method to assign a response to each number
-            String str = "";
-            str = getAnswer(number);
+            // Call magicEightBall method that will pick a random selection
+            // from the String array of possible responses
+            String str = magicEightBall();
 
             // Print the statements
+            System.out.println("YOU ASKED: " + userQuestion + "\n");
             System.out.println("MAGIC 8-BALL SAYS: " + str + "\n");
 
             // Ask the user if they want to play again
@@ -44,74 +40,27 @@ public class magicEightBall {
         System.out.println("Thank you for playing!");
         input.close();
         System.exit(0);
-    }
-
-    public static String getAnswer(int number){
-        String str = "";
-        switch (number){
-            case 1:
-                str = "It is certain";
-                break;
-            case 2:
-                str = "It is decidedly so";
-                break;
-            case 3:
-                str = "Without a doubt";
-                break;
-            case 4:
-                str = "Yes definitely";
-                break;
-            case 5:
-                str = "You may rely on it";
-                break;
-            case 6:
-                str = "Ah I see it, yes";
-                break;
-            case 7:
-                str = "Most Likely";
-                break;
-            case 8:
-                str = "Outlook good";
-                break;
-            case 9:
-                str = "Yes";
-                break;
-            case 10:
-                str = "Signs point to yes";
-                break;
-            case 11:
-                str = "Reply hazy try again";
-                break;
-            case 12:
-                str = "Ask again later";
-                break;
-            case 13:
-                str = "Better not tell you now";
-                break;
-            case 14:
-                str = "Cannot predict now";
-                break;
-            case 15:
-                str = "Concentrate and ask again";
-                break;
-            case 16:
-                str = "Don't count on it";
-                break;
-            case 17:
-                str = "My reply is no";
-                break;
-            case 18:
-                str = "My sources say no";
-                break;
-            case 19:
-                str = "Outlook not so good";
-                break;
-            case 20:
-                str = "Very doubtful";
-                break;
-        }
-        return str;
-
 
     }
+
+
+    public static String magicEightBall(){
+
+        // Create an array of all possible responses
+        String[] str = new String[]{"It is certain", "It is decidedly so", "Without a Doubt", "Yes definitely",
+                "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it",
+                "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+                "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+                "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
+
+        // Generate a random number between 1 and 20
+        int number = 0;
+        Random rnd = new Random();
+        number = rnd.nextInt(20)+1;
+
+        // Get a random selection from the responses
+        return str[number];
+
+    }
+
 }
